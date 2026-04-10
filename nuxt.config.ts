e9080b8292620f780e08e19615d3f8cf60ec10b0
Enrico Antonello <eantonel@gmail.com>
2026-04-11 00:10:16 +0200

    analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,14 +5,18 @@ export default defineNuxtConfig({
     logs: { silent: true }
   },
   css: ["bootstrap/dist/css/bootstrap.min.css", "~/assets/css/main.css"],
-  modules: ["@nuxtjs/sitemap", "@nuxtjs/robots"],
+  modules: ["@nuxtjs/sitemap", "@nuxtjs/robots", "@nuxtjs/google-analytics"],
+  googleAnalytics: {
+    id: process.env.NUXT_PUBLIC_GOOGLE_ANALYTICS_ID
+  },
   runtimeConfig: {
     contentfulSpaceId: process.env.CONTENTFUL_SPACE_ID,
     contentfulAccessToken: process.env.CONTENTFUL_DELIVERY_TOKEN,
     contentfulEnvironment: process.env.CONTENTFUL_ENVIRONMENT || "master",
     contentfulContentType: process.env.CONTENTFUL_CONTENT_TYPE || "pageContent",
     public: {
-      siteUrl: process.env.NUXT_PUBLIC_SITE_URL || "https://example.com"
+      siteUrl: process.env.NUXT_PUBLIC_SITE_URL || "https://example.com",
+      umamiWebsiteId: process.env.NUXT_PUBLIC_UMAMI_WEBSITE_ID || "f51261f7-a3fd-4c00-843d-704edacb83dd"
     }
   },
   app: {
@@ -34,6 +38,10 @@ export default defineNuxtConfig({
         {
           src: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js',
           defer: true
+        },
+        {
+          src: 'https://cloud.umami.is/script.js','data-website-id': process.env.NUXT_PUBLIC_UMAMI_WEBSITE_ID,
+          defer: true,
         }
       ],      
       htmlAttrs: {
